2a531d0fac141fe8e8eba452a7cdfa17fae51999
Harsh10092000 <143779029+Harsh10092000@users.noreply.github.com>
2024-03-11 16:07:07 +0530

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { AuthContextProvider } from "./context/AuthContext.jsx";
 import { AdminContextProvider } from "./context/AdminContext.jsx";
+import TagManager from 'react-gtm-module'
+
+const tagManagerArgs = {
+  gtmId: 'GTM-XXXXXX'
+}
+
+TagManager.initialize(tagManagerArgs)
+
 ReactDOM.createRoot(document.getElementById("root")).render(
+  
   <AuthContextProvider>
     <AdminContextProvider>
       <App />
