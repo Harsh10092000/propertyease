84062226a8e04de26840aad6805d2a38a2c7c429
Harsh10092000 <143779029+Harsh10092000@users.noreply.github.com>
2024-03-19 11:16:50 +0530

    commit

--- src/pages/allproperties/AllProperties.jsx
+++ src/pages/allproperties/AllProperties.jsx
@@ -66,16 +66,25 @@ const AllProperties = (props) => {
     });
   }, [data]);
 
+  //const [items , setItems] = useState("");
   var origin_url = document.referrer;
-  console.log("origin_url 1111111 : " , origin_url )
+  //console.log("origin_url 1111111 : " , origin_url )
   useEffect(() => {
+    //localStorage.setItem('origin_url', JSON.stringify("ghjgh"));  
+    const items = JSON.parse(localStorage.getItem('origin_url'));
+    console.log("items : " , items , origin_url)
+    if (items === null) {
+    //setItems(items);
     
     if(origin_url.startsWith("https://propertyease.in/") === false && origin_url.startsWith("https://www.propertyease.in/") === false && origin_url !== null && origin_url !== "") {
+      localStorage.setItem('origin_url', JSON.stringify(origin_url));  
       axios.post(import.meta.env.VITE_BACKEND + "/api/pro/addOrigin", [origin_url]);
       //document.referrer = null;
       console.log("origin_url 22222 : " , origin_url )
     } 
+  }
   }, []);
+  //console.log("local storage items : " , items);
 
   const [searchValue, setSearchValue] = useState("");
   const [filter, setFilter] = useState("All");
