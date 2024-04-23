c0a4f08771c867222eaa5be357381b1e159d691c
Harsh10092000 <143779029+Harsh10092000@users.noreply.github.com>
2024-04-23 11:13:41 +0530

    Update AdSlider2.jsx

--- src/components/adslider/AdSlider2.jsx
+++ src/components/adslider/AdSlider2.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import useEmblaCarousel from "embla-carousel-react";
 import "./adslider2.css";
+
 import Autoplay from "embla-carousel-autoplay";
 
 const AdSlider2 = (props) => {
